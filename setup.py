e0abe0040527081af20a092ead9d8fbb7643c7fa
dmegahan <dpm68@drexel.edu>
2013-04-22 16:34:14 -0400

    Added in original contributor, sorry!

--- setup.py
+++ setup.py
@@ -72,13 +72,13 @@ version = __import__(BASE_PACKAGE).get_version()
 
 setup(
     version = version,
-    name = 'django-redcapJSON',
-    author = 'Daniel Megahan',
-    author_email = 'megahand@email.chop.edu',
+    name = 'django-redcap',
+    author = 'Byron Ruth', 'Daniel Megahan',
+    author_email = 'ruthb@email.chop.edu', 'megahand@email.chop.edu',
     description = 'Utilities for porting REDCap data dictionaries to a JSON schema and a JSON schema to Django',
     license = 'BSD',
     keywords = 'redcap JSON django utils',
-    url = 'https://github.com/dmegahan/django-redcap/',
+    url = 'https://github.com/cbmi/django-redcap/',
 
     packages = packages,
     cmdclass = cmdclasses,
